e901f7e33f6dc25ac12697aea8942f2bda073d2d
ericghara <eric.gharakhanian@gmail.com>
2023-03-17 17:00:36 -0700

    config * Redis and test containers config complete

--- src/main/java/com/ericgha/config/RedisConfiguration.java
+++ src/main/java/com/ericgha/config/RedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.ericgha.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,16 +11,22 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 class RedisConfiguration {
 
+    @Value("${spring.data.redis.host}")
+    String redisHostname;
+    @Value("${spring.data.redis.port}")
+    Integer redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-
-        // todo parameterize hostname/port
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        // not currently required as all properties currently are autoconfigurable, but leaving open
+        // for future customization.
+        // To switch to jedis remember to switch client type in spring.data.redis.client-type
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHostname, redisPort);
         return new JedisConnectionFactory(config);
     }
 
     @Bean
-    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<String, String> redisTemplate(JedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         return template;
--- src/test/java/com/ericgha/service/RedisBackendCacheIntTest.java
+++ src/test/java/com/ericgha/service/RedisBackendCacheIntTest.java
@@ -1,10 +1,13 @@
 package com.ericgha.service;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -12,44 +15,28 @@ import org.testcontainers.utility.DockerImageName;
 
 import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 @Testcontainers
-@DataRedisTest
+@DataRedisTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = RedisBackendCache.class))
 public class RedisBackendCacheIntTest {
 
-    @Autowired
-    private RedisTemplate<String,String> redisTemplate;
-
-    private RedisBackendCache cache;
     @Container
-    public GenericContainer redis = new GenericContainer( DockerImageName.parse("redis:5.0.3-alpine"))
+    private static final GenericContainer<?> redis = new GenericContainer<>( DockerImageName.parse("redis:5.0.3-alpine"))
             .withExposedPorts(6379);
 
-    @BeforeEach
-    public void setUp() {
-        String address = redis.getHost();
-        Integer port = redis.getFirstMappedPort();
-
-        // Now we have an address and port for Redis, no matter where it is running
-        cache = new RedisBackendCache( redisTemplate );
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
     }
 
-    @Test
-    public void testSimplePutAndGet() {
-//        underTest.put("test", "example");
-//
-//        String retrieved = underTest.get("test");
-//        assertThat(retrieved).isEqualTo("example");
-        assertTrue(true);
-    }
+    @Autowired
+    private RedisBackendCache cache;
 
     @Test
     public void testPut() {
         String key = "Test Key";
         String expected = UUID.randomUUID().toString();
         cache.put(key, expected);
-        assertEquals(expected, cache.get(key));
+        Assertions.assertEquals( expected, cache.get(key) );
     }
 }
