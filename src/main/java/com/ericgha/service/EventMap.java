bd4cbb5d6961666899013e56e8e337df9661f5cf
ericghara <eric.gharakhanian@gmail.com>
2023-03-18 18:33:34 -0700

    feat: conditional event write * not working, save checkpoint

---
+++ src/main/java/com/ericgha/service/EventMap.java
@@ -0,0 +1,76 @@
+package com.ericgha.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.temporal.TemporalField;
+import java.util.Objects;
+
+@Service
+public class EventMap {
+
+    private final ValueOperations<String, String> valueOps;
+
+    // consider making constructor parameter, so you can validate;
+    @Value("${app.event-duration-millis}")
+    private int eventDurationMillis;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    EventMap(RedisTemplate<String,String> redisTemplate) {
+        this.valueOps = redisTemplate.opsForValue();
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void setEventDuration(int millis) {
+        this.eventDurationMillis = millis;
+    }
+
+    class ConditionalWrite implements SessionCallback<Boolean> {
+
+        private final String key;
+        private final long timestamp;
+
+        ConditionalWrite(String key, long timestamp) {
+            this.key = key;
+            this.timestamp = timestamp;
+        }
+
+        @Override
+        public Boolean execute(RedisOperations operations) throws DataAccessException {
+            operations.multi();
+            Object timeStr = operations.opsForValue().get(key);
+            long curTime = Instant.now().toEpochMilli();
+            if (Objects.isNull(timeStr) || Long.parseLong(timeStr.toString())+eventDurationMillis < curTime) {
+                operations.opsForValue().set( key, Long.toString(timestamp) );
+                operations.exec();
+                return true;
+            }
+            return false;
+        }
+    }
+
+    void put(String key, String value) {
+        valueOps.set(key, value);
+    }
+
+    public boolean putEvent(String event) {
+        ConditionalWrite cw = new ConditionalWrite( event, Instant.now().toEpochMilli() );
+        return cw.execute( redisTemplate );
+    }
+
+    public String get(String key) {
+        return valueOps.get(key);
+    }
+
+}
--- src/test/java/com/ericgha/service/EventMapIntTest.java
+++ src/test/java/com/ericgha/service/EventMapIntTest.java
@@ -1,11 +1,13 @@
 package com.ericgha.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -16,8 +18,8 @@ import org.testcontainers.utility.DockerImageName;
 import java.util.UUID;
 
 @Testcontainers
-@DataRedisTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = RedisBackendCache.class))
-public class RedisBackendCacheIntTest {
+@DataRedisTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = EventMap.class))
+public class EventMapIntTest {
 
     @Container
     private static final GenericContainer<?> redis = new GenericContainer<>( DockerImageName.parse("redis:5.0.3-alpine"))
@@ -27,16 +29,47 @@ public class RedisBackendCacheIntTest {
     static void properties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.redis.host", redis::getHost);
         registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+        registry.add("app.event-duration-millis", () -> 50_000_000);
     }
 
     @Autowired
-    private RedisBackendCache cache;
+    private EventMap eventMap;
+    @Autowired
+    RedisConnectionFactory connectionFactory;
+
+    @AfterEach
+    public void afterEach() {
+        connectionFactory.getConnection().commands().flushAll();
+    }
 
     @Test
     public void testPut() {
         String key = "Test Key";
         String expected = UUID.randomUUID().toString();
-        cache.put(key, expected);
-        Assertions.assertEquals( expected, cache.get(key) );
+        eventMap.put(key, expected);
+        Assertions.assertEquals( expected, eventMap.get(key) );
+    }
+
+    @Test
+    public void testGetNoKey() {
+        String key = "Test Key";
+        String found = eventMap.get(key);
+        Assertions.assertNull(found);
+    }
+
+    @Test
+    public void testPutEventNoDuplicate() {
+        String event = "testEvent";
+        boolean found = eventMap.putEvent(event);
+        Assertions.assertTrue(found);
+    }
+
+    @Test
+    public void testPutEventBlockingDuplicate() {
+        String event = "testEvent";
+        boolean foundFirst = eventMap.putEvent(event);
+        boolean foundSecond = eventMap.putEvent(event);
+        Assertions.assertTrue(foundFirst);
+        Assertions.assertFalse(foundSecond);
     }
 }
